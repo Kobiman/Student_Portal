2d17c3e2d1d01632d9006397c7d6f39d664e0368
Ackon Jude Peter <astromyllc@gmail.com>
2021-03-03 14:37:52 +0000

    Check Result BLL

---
+++ Sp.Commands/CheckResultCommand.cs
@@ -0,0 +1,37 @@
+﻿using SP.Common;
+using SP.Models;
+using SP.Models.Dtos;
+using SP.Services.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using KMapper;
+using SP.Services.Interfaces;
+using System.Security.Cryptography.X509Certificates;
+using Sp.Commands;
+
+namespace Sp.Commands
+{
+    class CheckResultCommand : ICheckResultCommand
+    {
+        private readonly IUnitOfWork _uow;
+        public CheckResultCommand(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        public IResult Execute(IRequest student)
+        {
+            if (student is AddStudentRequest getStudentRequest) 
+            {
+            ICollection<ValidationResult> validationResults = new List<ValidationResult>();
+            if (!student.Validate(out validationResults)) return new Result(false, validationResults.First().ErrorMessage);
+            var studResult = _uow.ExamResults.GetByStudent(getStudentRequest.IndexNumber);
+            if (studResult == null) return new Result(false, "Student Not Found");
+            return new Result<IList<StudentResult>>(true, studResult, "");
+        }
+             return new Result(true, "");
+    }
+    }
+}
---
+++ Sp.Commands/ICheckResultCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SP.Services.Interfaces.Commands;
+
+namespace Sp.Commands
+{
+    internal interface ICheckResultCommand : ICommand
+    {
+    }
+}
